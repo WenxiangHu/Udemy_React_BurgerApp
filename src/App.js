1b4c96b7c2d6fb51c872f27b0ade26295849683e
Wenxiang Hu <winsonwho@gmail.com>
2020-06-28 14:35:33 -0700

    set up and create layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout/Layout'
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        Hello World
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
