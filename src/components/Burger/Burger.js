9f4ee4c3266d8d5c9871e823d01931da773fc4c0
Wenxiang Hu <winsonwho@gmail.com>
2020-06-28 15:39:33 -0700

    Dynamic Burger Component, trans object to array.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import './Burger.css'
+
+const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) => {  //flatten of array
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
+
+
+    return (
+        <div className='Burger'>
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+
+    )
+}
+
+export default Burger
