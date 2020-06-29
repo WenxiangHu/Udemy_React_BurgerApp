5c17cdaf590c4909d3053eb3de2fb657235ba78e
Wenxiang Hu <winsonwho@gmail.com>
2020-06-28 17:58:45 -0700

    add/remove ingredients done

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,19 +3,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css'
 
 const Burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />
-            });
+    let transformedIngredients = Object.keys(props.ingredients).map( //[salad, meat, ..]
+        ingredientName => {
+            return [...Array(props.ingredients[ingredientName])] //create empty array with size of required
+                .map((_, i) => {  //any value of element in array, index of the element
+                    return <BurgerIngredient key={ingredientName + i} type={ingredientName} />
+                })
         })
         .reduce((arr, el) => {  //flatten of array
             return arr.concat(el)
         }, []);
+    // console.log(transformedIngredients)  //Can check before/after reduce()
+
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please Start Adding Ingredients</p>
     }
-
 
     return (
         <div className='Burger'>
@@ -28,3 +30,4 @@ const Burger = (props) => {
 }
 
 export default Burger
+
